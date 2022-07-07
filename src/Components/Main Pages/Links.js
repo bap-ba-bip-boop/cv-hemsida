43a9a7d5a9f0ad35671d2f0e918526a26475747e
Calle <calle.be@telia.com>
2022-07-07 11:08:02 +0200

    asd

---
+++ src/Components/Main Pages/Links.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Links = () => {
+  return (
+    <>
+        <a href={""}>linkedin</a>
+        <a href={"https://github.com/bap-ba-bip-boop"}>github</a>
+    </>
+  )
+}
