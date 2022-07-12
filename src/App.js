8697c7da1cb73ea6e0b3bfef386ce25007c3e280
Calle <calle.be@telia.com>
2022-07-12 15:24:37 +0200

    message

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ export const App = () => {
 
   const [activePage, SetActivePage] = useState(appSettings.pages[0].Page);
 
-  console.log("start page value: " + appSettings.pages[0].Page);
+  
 
   const changeActivePage = input =>
   {
     SetActivePage(input);
+    console.log("new Page: " + input);
   }
 
   return (
