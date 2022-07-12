7c874aac2b615ac3e1b69b44aefe4253f186b946
Calle <calle.be@telia.com>
2022-07-12 22:01:15 +0200

    some skills css

--- src/Components/Main Pages/Skills.js
+++ src/Components/Main Pages/Skills.js
@@ -7,30 +7,30 @@ import { ITStuffComponent } from './Main Components/ITStuffComponent';
 export const Skills = () => {
   return (
     <>
-        <section>
-            <h3>{appSettings.languageTitle}</h3>
-            {
-            appSettings.spokenLanguages.map((language,i) =>
-              <LanguageComponent
+      <section>
+          <h2 className='skillsTitle'>{appSettings.ITStuffTitle}</h2>
+          {
+          appSettings.ITStuff.map( (category,i) =>
+              <ITStuffComponent
               key={i}
-              languageName={language.languageName}
-              level={language.level}
+              CategoryName={category.CategoryName}
+              items={category.items}
               />
-              )
-            }
-        </section>
-        <section>
-            <h3>{appSettings.ITStuffTitle}</h3>
-            {
-            appSettings.ITStuff.map( (category,i) =>
-                <ITStuffComponent
-                key={i}
-                CategoryName={category.CategoryName}
-                items={category.items}
-                />
-              )
-            }
-        </section>
+            )
+          }
+      </section>
+      <section>
+          <h2 className='skillsTitle'>{appSettings.languageTitle}</h2>
+          {
+          appSettings.spokenLanguages.map((language,i) =>
+            <LanguageComponent
+            key={i}
+            languageName={language.languageName}
+            level={language.level}
+            />
+            )
+          }
+      </section>
     </>
   )
 }
